a0cdf392cfcb8ac518de52ca21586f93b26b2b1c
axim <maxim2k18gh8hg@gmail.com>
2023-11-13 10:32:49 +0200

    upd hw1

---
+++ hw_1/task_1.py
@@ -0,0 +1,3 @@
+text = "Nothing will work unless you do"
+list_ = text.split(" ")
+print("%s\n%s %s\n%s %s %s" % (list_[0], list_[1], list_[2], list_[3], list_[4], list_[5]))
---
+++ hw_1/task_2.py
@@ -0,0 +1,8 @@
+quote = '"Anyone who\nstops\nlearning is old,\nwhether at twenty or eighty"'
+author = "\nhenry ford".title()
+print(quote, author)
+
+
+
+
+
